3a9af0df4fbde46008bdbe71f8607eb30ff3eec3
Sijmen Duineveld <s.a.duineveld@outlook.com>
2024-10-10 11:45:43 +0200

    Add grid

--- BM_mod.py
+++ BM_mod.py
@@ -1,5 +1,21 @@
 # BM model module
 
+def get_res(alpha,beta,lk_vec,pol_old,lc_pol)
+    kt = math.exp(lk_vec)
+    kn = knext(alpha,kt,lc_pol)
+    lkn = math.log(kn)
+    lcn = pol_old(lkn)
+    Rn = alpha*kn^(alpha-1)
+    RES = lc_pol
+    return RES
+
+
+
+def get_ss(alpha,beta):
+    kss = (alpha*beta)**(1/(1-alpha))
+    css = kss**alpha - kss
+    return kss,css
+
 
 def marg_ut(nu,cc):
     dudc = cc**-nu
--- run_BM_tests.py
+++ run_BM_tests.py
@@ -1,14 +1,26 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from scipy.interpolate import RectBivariateSpline
 
-import BM_mod
+import BM_mod as BM
 
 k = 2
 alpha = 0.33
+beta = 0.96
 
-y = BM_mod.prod(alpha,k)
+y = BM.prod(alpha,k)
 
 print(k)
 print(alpha)
 print(y)
+
+# Construct grid
+nodes = 5
+kss,css = BM.get_ss(alpha,beta)
+lk_vec = np.linspace(math.log(kss)-0.2,math.log(kss)+0.2,nodes)
+
+# Initial guess
+lc_pol = math.log(css)+0.01*(lk_vec - math.log(kss))
+print(lc_pol)
+
